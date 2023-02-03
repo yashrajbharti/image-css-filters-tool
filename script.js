adcc617999317cee66fe1896f9615db6196ac8ed
Yash Raj <Yash@google.com>
2023-02-03 12:22:46 +0530

    removed html2canvas and filesaver libraries and now the code uses vanilla javascript and canvas to work

--- script.js
+++ script.js
@@ -11,34 +11,73 @@ let saturate = 100;
 let brightness = 105;
 let contrast = 105;
 
+var canvas = document.getElementById("c");
+var ctx = canvas.getContext("2d");
+var img = new Image();
+
+//Image with EventListener
+img.addEventListener(
+  "load",
+  function () {
+    ctx.filter = "none";
+    ctx.drawImage(img, 0, 0, 556, 162);
+    window.URL.revokeObjectURL(this.src);
+  },
+  false
+);
+img.src = "stage.png";
+img.style.width = `${556}px`;
+img.style.height = `${162}px`;
+img.id = "stage";
+//<img id="stage" src="stage.png" alt="" width="100%">
+
 slider.oninput = function () {
   output.innerHTML = this.value;
   hue = this.value;
-  document.getElementById(
-    "stage"
-  ).style.filter = `hue-rotate(${hue}deg) saturate(${saturate}%) brightness(${brightness}%) contrast(${contrast}%)`;
+  change();
 };
 
 slider2.oninput = function () {
   output2.innerHTML = this.value;
   saturate = this.value;
-  document.getElementById(
-    "stage"
-  ).style.filter = `hue-rotate(${hue}deg) saturate(${saturate}%) brightness(${brightness}%) contrast(${contrast}%)`;
+  change();
 };
 
 slider3.oninput = function () {
   output3.innerHTML = this.value;
   brightness = this.value;
-  document.getElementById(
-    "stage"
-  ).style.filter = `hue-rotate(${hue}deg) saturate(${saturate}%) brightness(${brightness}%) contrast(${contrast}%)`;
+  change();
 };
 
 slider4.oninput = function () {
   output4.innerHTML = this.value;
   contrast = this.value;
-  document.getElementById(
-    "stage"
-  ).style.filter = `hue-rotate(${hue}deg) saturate(${saturate}%) brightness(${brightness}%) contrast(${contrast}%)`;
+  change();
 };
+
+var download = document.getElementById("download");
+let huecopy = hue;
+let saturatecopy = saturate;
+let brightnesscopy = brightness;
+let contrastcopy = contrast;
+download.addEventListener(
+  "click",
+  function () {
+    ctx.filter = `hue-rotate(${hue}deg) saturate(${saturate}%) brightness(${brightness}%) contrast(${contrast}%)`;
+    ctx.drawImage(img, 0, 0, 556, 162);
+    download.href = canvas.toDataURL();
+    download.download = "img.png";
+    resetImage();
+  },
+  false
+);
+
+function change() {
+  document.getElementById(
+    "c"
+  ).style.filter = `hue-rotate(${hue}deg) saturate(${saturate}%) brightness(${brightness}%) contrast(${contrast}%)`;
+}
+
+function resetImage() {
+  img.src = "stage.png";
+}
